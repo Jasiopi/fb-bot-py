e0c3c58ef340bb13f14643856672038f9d6b3692
Jasiopi <pieprzycki.jan@gmail.com>
2024-05-24 21:41:59 +0200

    małe piwko

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ print(v)
 
 driver = webdriver.Firefox()
 driver.implicitly_wait(10)
-driver.get("https://www.facebook.com/wojtGminyIzabelin/?locale=pl_PL")
+driver.get("https://www.facebook.com/InOManiak")
 
 time.sleep(5)
 
@@ -57,3 +57,4 @@ driver.quit()
 #/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div[4]/div[2]/div/div[2]/div/div[6]
 #/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div/div[1]
 #/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div/div[5]
+#piwo
